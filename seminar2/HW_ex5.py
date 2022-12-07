b1f561a8e317e7bf190d7b09de75a902714933c1
Max <makism.mush21@gmail.com>
2022-12-07 11:55:31 +0300

    fix ex_5

--- seminar2/HW_ex5.py
+++ seminar2/HW_ex5.py
@@ -1,14 +1,27 @@
 # Реализуйте алгоритм перемешивания списка.
 
-import random
+from random import randint
+
+def mix(lst):
+    for i in range(len(lst)):
+        new_i = randint(0, len(lst) - 1)
+        lst[i], lst[new_i] = lst[new_i], lst[i]
+        # tmp = lst[i]
+        # lst[i] = lst[new_i]
+        # lst[new_i] = tmp
+
+lst_ = [1, 2, 3, 4, 5]
+mix(lst_)
+print(lst_)
 
 
-size = int(input('Размер списка: '))
-lst_2 = []
 
-for i in range(1, size + 1):
-    random.randint(1, size)
-    lst_2.append(random.randint(1, size))
+# size = int(input('Размер списка: '))
+# lst_2 = []
 
-print(lst_2)
+# for i in range(1, size + 1):
+#     random.randint(1, size)
+#     lst_2.append(random.randint(1, size))
+
+# print(lst_2)
 
